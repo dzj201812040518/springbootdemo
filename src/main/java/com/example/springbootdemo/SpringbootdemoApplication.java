fa0c95092c4df1dc51c9fc021a2727c7cad55f8a
dzj <dzj201812040518@163.com>
2020-08-22 14:04:33 +0800

    实例项目+SQL文件

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -3,9 +3,12 @@ package com.example.springbootdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 启动类
+ * @author xiaojie
+ * 此处必须加入MapperScan注解，前面根据自己项目来更改即可
+ */
 @SpringBootApplication
-//此处必须加入MapperScan注解，前面根据自己项目来更改即可
 @MapperScan("com.example.springbootdemo.**.mapper")
 public class SpringbootdemoApplication {
 
