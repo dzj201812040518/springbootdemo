206f700150c8dfa46dcb9dfdaba10192b7b6d698
dzj <dzj201812040518@163.com>
2020-08-22 14:03:02 +0800

    实例项目+SQL文件

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//此处必须加入MapperScan注解，前面根据自己项目来更改即可
 @MapperScan("com.example.springbootdemo.**.mapper")
 public class SpringbootdemoApplication {
 
